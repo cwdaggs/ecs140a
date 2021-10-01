b035c9ec4921864224ef7638fed31821073b0cc5
cwdaggs <cwdaggs@ucdavis.edu>
2021-10-01 10:40:43 -0700

    Awful loop

--- proj1/rpn.h
+++ proj1/rpn.h
@@ -2,42 +2,8 @@
 #include <vector>
 #include <stack>
 #include <math.h>
+#include <array>
 using namespace std;
-
-double rpn(string strs[]) {
-	double result = 0.0;
-
-	std::stack<float> nums;
-	for (unsigned int i = 0; i < sizeof(strs)/sizeof(strs[0]); i++) {
-		if (isNum(strs[i])) {
-			nums.push(stof(strs[i]));
-		} else {
-			float first_operand = nums.top();
-			nums.pop();
-			if (isFloor(strs[i])) {
-				nums.push(floor(first_operand));
-			} else if (isCeiling(strs[i])) {
-				nums.push(ceil(first_operand));
-			} else {
-				float second_operand = nums.top();
-				nums.pop();
-				if (isAddition(strs[i])) {
-					nums.push(first_operand + second_operand);
-				} else if (isSubtraction(strs[i])) {
-					nums.push(first_operand - second_operand);
-				} else if (isMultiplication(strs[i])) {
-					nums.push(first_operand * second_operand);
-				} else if (isDivision(strs[i])) {
-					nums.push(first_operand / second_operand);
-				} else if (isExponent(strs[i])) {
-					nums.push(pow(first_operand, second_operand));
-				}
-			}
-		}
-	}
-	result += nums.top();
-	return result;
-}
 
 bool isNum(string val) {
 	if (int(val[0]) >= 48 && int(val[0]) < 58) {
@@ -69,3 +35,39 @@ bool isFloor(string op) {
 bool isCeiling(string op) {
 	return (int(op[0]) == 62);
 }
+
+double rpn(string strs[]) {
+	double result = 0.0;
+
+	std::stack<float> nums;
+	for (unsigned int i = 0; i < strs->size(); i++) {
+		if (isNum(strs[i])) {
+			nums.push(stof(strs[i]));
+		} else {
+			float first_operand = nums.top();
+			nums.pop();
+			if (isFloor(strs[i])) {
+				nums.push(floor(first_operand));
+			} else if (isCeiling(strs[i])) {
+				nums.push(ceil(first_operand));
+			} else {
+				float second_operand = nums.top();
+				nums.pop();
+				if (isAddition(strs[i])) {
+					nums.push(first_operand + second_operand);
+				} else if (isSubtraction(strs[i])) {
+					nums.push(first_operand - second_operand);
+				} else if (isMultiplication(strs[i])) {
+					nums.push(first_operand * second_operand);
+				} else if (isDivision(strs[i])) {
+					nums.push(first_operand / second_operand);
+				} else if (isExponent(strs[i])) {
+					nums.push(pow(first_operand, second_operand));
+				}
+			}
+		}
+	}
+	result += nums.top();
+	return result;
+}
+
